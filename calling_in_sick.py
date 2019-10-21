b235a465f2eb7d3e01c6afa4d7c604163f5ac386
Tshepo <teemo.dev@hotmail.com>
2019-10-21 15:43:09 +0200

    Calling in sick
    Depending on conditions, checks whether you should call in sick

---
+++ calling_in_sick.py
@@ -0,0 +1,20 @@
+# NO TOUCHING ======================================
+
+from random import choice, randint
+
+# randomly assigns values to these four variables
+actually_sick = choice([True, False])
+kinda_sick = choice([True, False])
+hate_your_job = choice([True, False])
+sick_days = randint(0, 10)
+
+calling_in_sick = None  # set this to True or False with Boolean Logic and Conditionals!
+
+if actually_sick and (sick_days > 0 or sick_days <= 10):
+    calling_in_sick = True
+elif kinda_sick and hate_your_job and (sick_days > 0 or sick_days <= 10):
+    calling_in_sick = True
+else:
+    calling_in_sick = False
+
+print(f"Are you calling in sicK? {calling_in_sick}")
