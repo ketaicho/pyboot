0bdcd96e06b4bcc67cc7aaecc52ef0d3a242f51b
Tshepo <teemo.dev@hotmail.com>
2019-11-06 23:07:55 +0200

    Guessing game
    Proper working solution

--- guessing_game.py
+++ guessing_game.py
@@ -1,29 +1,33 @@
 from random import randint
 
 rand_num = randint(1,10)
-print(f"NUMBER: {rand_num}\n")
 
-play = 'y'
-cont = 'n'
+play = "y"
+cont = "n"
+guess = 0
 
-guess = input("Guess a number between 1 and 10: ")
-guess = int(guess) #need some sanitizing/error-checking here 
-
-while play == 'y':
-    if (guess >= 0) and (guess <= 10):
+while play == "y":
+    guess = input("Guess a number between 1 and 10: ")
+    guess = int(guess) #need some sanitizing/error-checking here 
+    
+    if (guess >= 1) and (guess <= 10):
         if guess > rand_num:
             print("Too high, try again!")
         elif guess < rand_num:
             print("Too low, try again!")
         else:
             print("You guessed it! You WON!")
-        cont = input("Do you want to keep playing? ")
-    else:
-        print("Please enter a valid response!")
-    
-    if (cont != 'y') or (cont != 'n') or (cont == 'n'):
-        play = 'n'
-    else:
-        play = 'y'
 
-print("\n") 
+            cont = input("Do you want to keep playing? (y/n) ")
+            print("\n")
+            
+            if cont == "y":
+                play = "y"
+                rand_num = randint(1,10)
+            elif (cont != "y") or (cont != "n") or (cont == "n"):
+                play = "n"
+
+    else:
+        print("Please enter a valid response! \n")
+
+print("\n")
