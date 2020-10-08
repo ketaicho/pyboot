9c9e10557e63260c2efa7b149061d0677f8330a7
Tshepo <teemo.dev@hotmail.com>
2020-10-08 22:50:25 +0200

    Params demo func

---
+++ def_params.py
@@ -0,0 +1,10 @@
+def speak(animal="dog"):
+    if animal=="pig":
+        return "oink"
+    elif animal=="duck":
+        return "quack"
+    elif animal=="cat":
+        return "meow"
+    elif animal=="dog":
+        return "woof"
+    return "?"
