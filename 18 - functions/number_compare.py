2b1f23972587dbccb15475a5c745d9138b692e46
Tshepo <teemo.dev@hotmail.com>
2020-10-19 20:17:37 +0200

    fromkeys exercise

--- 18 - functions/number_compare.py
+++ 18 - functions/number_compare.py
@@ -1,11 +1,16 @@
 # function to compare two numbers and return greater number
 
 num1 = input("Enter first number: ")
-num2 = input("Enter seconmd number: ")
+num2 = input("Enter second number: ")
 
-# this approach used to demonstrate 'ternary conditional'
-# also, this solution negates the different conditions to cater for
 def number_compare(num1=0, num2=0):
-    return "First is greater" if (num1 > num2) else "Second is greater"
+    if (num1 > num2):
+        return "First is greater"
+    elif (num2 > num1):
+        return "Second is greater"
+    elif (num1 == num2):
+        return "Numbers are equal"
+    else:
+        "Please enter valid selection"
 
 print(number_compare(num1, num2))
