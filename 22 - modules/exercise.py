71e9d7885b67f5ddc091074833e7d54feec05f75
Tshepo <teemo.dev@hotmail.com>
2020-11-23 21:55:13 +0200

    function to play around with importing custom modules

---
+++ 22 - modules/exercise.py
@@ -0,0 +1,6 @@
+# testing out custom modules
+# import the helpers module. call the function in the module
+import helpers
+
+num = helpers.lucky_number()
+print(num)
---
+++ 22 - modules/helpers.py
@@ -0,0 +1,5 @@
+# testing out custom modules
+# function that always returns the number 37
+
+def lucky_number():
+    return 37
