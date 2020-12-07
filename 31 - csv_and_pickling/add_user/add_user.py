f0531e02d622bf4745c5e355291ce9c9a8b390a2
Tshepo <teemo.dev@hotmail.com>
2020-12-07 23:27:53 +0200

    Function to append names to the end of a csv file

--- 31 - csv_and_pickling/add_user/add_user.py
+++ 31 - csv_and_pickling/add_user/add_user.py
@@ -1,2 +1,18 @@
 # function that takes in a first name and last name, adds a new user to csv file
+from csv import DictWriter
 
+def add_user(firstn, lastn):
+    with open('users.csv', 'a', newline='') as csvfile:
+        fields = ['First Name', 'Last Name']
+        writer = DictWriter(csvfile, fieldnames=fields)
+
+        #writer.writeheader()
+        writer.writerow({'First Name': firstn, 'Last Name': lastn})
+
+
+fname = input("Enter first name: ")
+lname = input("Enter last name: ")
+
+add_user(fname,lname)
+
+print('Done!')
