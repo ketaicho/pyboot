470145cff73478040a7fda042a19e6f14ca358f4
Tshepo <teemo.dev@hotmail.com>
2020-12-19 14:43:13 +0200

    Simple class exercise

---
+++ 24 - oop/comment.py
@@ -0,0 +1,9 @@
+class Comment():
+    def __init__(self, username, text="", likes=0):
+        self.username = username
+        self.text = text
+        self.likes = likes
+
+com = Comment("davery134", "lol you're so silly!")
+print(com.likes)
+
