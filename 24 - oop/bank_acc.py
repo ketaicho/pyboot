2623c67eedaf105ff15e3a8432d59d8fd880b9b8
Tshepo <teemo.dev@hotmail.com>
2020-12-19 16:20:38 +0200

    Class of a bank account with simple methods

---
+++ 24 - oop/bank_acc.py
@@ -0,0 +1,26 @@
+class BankAccount():
+    def __init__(self, owner, balance=0):
+        self.owner = owner
+        self.balance = balance
+
+    def deposit(self, dep_amount):
+        if float(dep_amount):
+            self.balance = self.balance + dep_amount
+            return self.balance
+        else:
+            return "Please enter valid amount"
+
+    def withdraw(self, with_amount):
+        if float(with_amount):
+            if (self.balance - with_amount) < 0:
+                return "Insufficient funds available"
+            self.balance = self.balance - with_amount
+            return self.balance
+        else:
+            return "Please enter valid amount"
+
+acc = BankAccount("Tshepo")
+acc.deposit(20)
+print(acc.balance)
+print(acc.withdraw(25))
+print(acc.balance)
